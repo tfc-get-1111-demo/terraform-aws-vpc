60bface89eb527a4da9b38e5db40760371e9e2e9
Ryano0oceros <getryanfallon@gmail.com>
2021-05-18 18:36:44 -0400

    comment out deprecated list fn

--- outputs.tf
+++ outputs.tf
@@ -103,85 +103,85 @@ output "natgw_ids" {
   value       = aws_nat_gateway.this.*.id
 }
 
-output "igw_id" {
-  description = "The ID of the Internet Gateway"
-  value       = element(concat(aws_internet_gateway.this.*.id, list("")), 0)
-}
+# output "igw_id" {
+#   description = "The ID of the Internet Gateway"
+#   value       = element(concat(aws_internet_gateway.this.*.id, list("")), 0)
+# }
 
-output "vpc_endpoint_s3_id" {
-  description = "The ID of VPC endpoint for S3"
-  value       = element(concat(aws_vpc_endpoint.s3.*.id, list("")), 0)
-}
+# output "vpc_endpoint_s3_id" {
+#   description = "The ID of VPC endpoint for S3"
+#   value       = element(concat(aws_vpc_endpoint.s3.*.id, list("")), 0)
+# }
 
-output "vpc_endpoint_s3_pl_id" {
-  description = "The prefix list for the S3 VPC endpoint."
-  value       = element(concat(aws_vpc_endpoint.s3.*.prefix_list_id, list("")), 0)
-}
+# output "vpc_endpoint_s3_pl_id" {
+#   description = "The prefix list for the S3 VPC endpoint."
+#   value       = element(concat(aws_vpc_endpoint.s3.*.prefix_list_id, list("")), 0)
+# }
 
-output "vpc_endpoint_dynamodb_id" {
-  description = "The ID of VPC endpoint for DynamoDB"
-  value       = element(concat(aws_vpc_endpoint.dynamodb.*.id, list("")), 0)
-}
+# output "vpc_endpoint_dynamodb_id" {
+#   description = "The ID of VPC endpoint for DynamoDB"
+#   value       = element(concat(aws_vpc_endpoint.dynamodb.*.id, list("")), 0)
+# }
 
-output "vgw_id" {
-  description = "The ID of the VPN Gateway"
-  value       = element(concat(aws_vpn_gateway.this.*.id, aws_vpn_gateway_attachment.this.*.vpn_gateway_id, list("")), 0)
-}
+# output "vgw_id" {
+#   description = "The ID of the VPN Gateway"
+#   value       = element(concat(aws_vpn_gateway.this.*.id, aws_vpn_gateway_attachment.this.*.vpn_gateway_id, list("")), 0)
+# }
 
-output "vpc_endpoint_dynamodb_pl_id" {
-  description = "The prefix list for the DynamoDB VPC endpoint."
-  value       = element(concat(aws_vpc_endpoint.dynamodb.*.prefix_list_id, list("")), 0)
-}
+# output "vpc_endpoint_dynamodb_pl_id" {
+#   description = "The prefix list for the DynamoDB VPC endpoint."
+#   value       = element(concat(aws_vpc_endpoint.dynamodb.*.prefix_list_id, list("")), 0)
+# }
 
-output "default_vpc_id" {
-  description = "The ID of the VPC"
-  value       = element(concat(aws_default_vpc.this.*.id, list("")), 0)
-}
+# output "default_vpc_id" {
+#   description = "The ID of the VPC"
+#   value       = element(concat(aws_default_vpc.this.*.id, list("")), 0)
+# }
 
-output "default_vpc_cidr_block" {
-  description = "The CIDR block of the VPC"
-  value       = element(concat(aws_default_vpc.this.*.cidr_block, list("")), 0)
-}
+# output "default_vpc_cidr_block" {
+#   description = "The CIDR block of the VPC"
+#   value       = element(concat(aws_default_vpc.this.*.cidr_block, list("")), 0)
+# }
 
-output "default_vpc_default_security_group_id" {
-  description = "The ID of the security group created by default on VPC creation"
-  value       = element(concat(aws_default_vpc.this.*.default_security_group_id, list("")), 0)
-}
+# output "default_vpc_default_security_group_id" {
+#   description = "The ID of the security group created by default on VPC creation"
+#   value       = element(concat(aws_default_vpc.this.*.default_security_group_id, list("")), 0)
+# }
 
-output "default_vpc_default_network_acl_id" {
-  description = "The ID of the default network ACL"
-  value       = element(concat(aws_default_vpc.this.*.default_network_acl_id, list("")), 0)
-}
+# output "default_vpc_default_network_acl_id" {
+#   description = "The ID of the default network ACL"
+#   value       = element(concat(aws_default_vpc.this.*.default_network_acl_id, list("")), 0)
+# }
 
-output "default_vpc_default_route_table_id" {
-  description = "The ID of the default route table"
-  value       = element(concat(aws_default_vpc.this.*.default_route_table_id, list("")), 0)
-}
+# output "default_vpc_default_route_table_id" {
+#   description = "The ID of the default route table"
+#   value       = element(concat(aws_default_vpc.this.*.default_route_table_id, list("")), 0)
+# }
 
-output "default_vpc_instance_tenancy" {
-  description = "Tenancy of instances spin up within VPC"
-  value       = element(concat(aws_default_vpc.this.*.instance_tenancy, list("")), 0)
-}
+# output "default_vpc_instance_tenancy" {
+#   description = "Tenancy of instances spin up within VPC"
+#   value       = element(concat(aws_default_vpc.this.*.instance_tenancy, list("")), 0)
+# }
 
-output "default_vpc_enable_dns_support" {
-  description = "Whether or not the VPC has DNS support"
-  value       = element(concat(aws_default_vpc.this.*.enable_dns_support, list("")), 0)
-}
+# output "default_vpc_enable_dns_support" {
+#   description = "Whether or not the VPC has DNS support"
+#   value       = element(concat(aws_default_vpc.this.*.enable_dns_support, list("")), 0)
+# }
 
-output "default_vpc_enable_dns_hostnames" {
-  description = "Whether or not the VPC has DNS hostname support"
-  value       = element(concat(aws_default_vpc.this.*.enable_dns_hostnames, list("")), 0)
-}
+# output "default_vpc_enable_dns_hostnames" {
+#   description = "Whether or not the VPC has DNS hostname support"
+#   value       = element(concat(aws_default_vpc.this.*.enable_dns_hostnames, list("")), 0)
+# }
 
-//output "default_vpc_enable_classiclink" {
-//  description = "Whether or not the VPC has Classiclink enabled"
-//  value       = "${element(concat(aws_default_vpc.this.*.enable_classiclink, list("")), 0)}"
-//}
+# //output "default_vpc_enable_classiclink" {
+# //  description = "Whether or not the VPC has Classiclink enabled"
+# //  value       = "${element(concat(aws_default_vpc.this.*.enable_classiclink, list("")), 0)}"
+# //}
 
-output "default_vpc_main_route_table_id" {
-  description = "The ID of the main route table associated with this VPC"
-  value       = element(concat(aws_default_vpc.this.*.main_route_table_id, list("")), 0)
-}
+# output "default_vpc_main_route_table_id" {
+#   description = "The ID of the main route table associated with this VPC"
+#   value       = element(concat(aws_default_vpc.this.*.main_route_table_id, list("")), 0)
+# }
 
 //output "default_vpc_ipv6_association_id" {
 //  description = "The association ID for the IPv6 CIDR block"
